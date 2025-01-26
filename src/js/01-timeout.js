359ff40c2fe42bfdbf1b56a159491df7b6c45684
grypis <grypisnecros@gmail.com>
2025-01-26 04:09:05 +0200

    asd

--- src/js/01-timeout.js
+++ src/js/01-timeout.js
@@ -8,7 +8,7 @@ console.log(3);
 console.log(4); */
 
 //! ======================================
-console.log("Hello world");
+/* console.log("Hello world");
 
 function callback() {
   console.log("TEST");
@@ -22,4 +22,36 @@ setTimeout(() => {
 }, 2000);
 setTimeout(() => {
   console.log("Hello 3");
-}, 3000);
+}, 3000); */
+
+//! ======================================
+/* for (let i = 1; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, i * 1000);
+}
+ */
+
+/* for (let i = 4; i > 0; i--) {
+  setTimeout(callback, i * 1000, i, 1, 2);
+}
+
+function callback(value, x, y) {
+  console.log(value, x, y);
+}
+ */
+
+//! ======================================
+/* const id = setTimeout(() => {
+  console.log("Hello world");
+}, 7000);
+
+setTimeout(() => {
+  clearTimeout(id);
+  console.log("Remove");
+}, 2000); */
+
+/* const timeoutId = setTimeout(() => {}, 0);
+console.log(timeoutId);
+
+clearTimeout(timeoutId); */
